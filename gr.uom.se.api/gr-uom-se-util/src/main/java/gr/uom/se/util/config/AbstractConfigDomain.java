b63cf96e25b7cf36ca3bb1394ce2366e9a74beaa
elvis <elvis_ligu@hotmail.com>
2015-02-13 02:41:32 +0200

    Removing eclipse files

--- gr.uom.se.api/gr-uom-se-util/src/main/java/gr/uom/se/util/config/AbstractConfigDomain.java
+++ gr.uom.se.api/gr-uom-se-util/src/main/java/gr/uom/se/util/config/AbstractConfigDomain.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package gr.uom.se.util.config;
 
@@ -13,78 +13,80 @@ import java.util.concurrent.ConcurrentHashMap;
  * Uses internally a concurrent hash map to obtain values. If a null value is
  * specified for a property it will remove the old value if any. That means null
  * values are allowed.
- * 
+ *
  * @author Elvis Ligu
  * @version 0.0.1
  * @since 0.0.1
  */
 public abstract class AbstractConfigDomain implements ConfigDomain {
 
-   /**
-    * The domain name of this configuration.
-    * <p>
-    */
-   protected final String name;
+    /**
+     * The domain name of this configuration.
+     * <p>
+     */
+    protected final String name;
 
-   /**
-    * The properties of this domain.
-    * <p>
-    * The map is a concurrent map.
-    */
-   protected ConcurrentHashMap<String, Object> properties;
+    /**
+     * The properties of this domain.
+     * <p>
+     * The map is a concurrent map.
+     */
+    protected ConcurrentHashMap<String, Object> properties;
 
-   /**
-    * Create an instance with the given name.
-    * <p>
-    */
-   public AbstractConfigDomain(String name) {
-      ArgsCheck.notEmpty("name", name);
-      this.name = name;
-      this.properties = new ConcurrentHashMap<>();
-   }
+    /**
+     * Create an instance with the given name.
+     * <p>
+     */
+    public AbstractConfigDomain(String name) {
+        ArgsCheck.notEmpty("name", name);
+        this.name = name;
+        this.properties = new ConcurrentHashMap<>();
+    }
 
-   /**
-    * {@inheritDoc)
-    */
-   @Override
-   public String getName() {
-      return name;
-   }
+    /**
+     * {@inheritDoc)
+     *
+     */
+    @Override
+    public String getName() {
+        return name;
+    }
 
-   /**
-    * {@inheritDoc)
-    */
-   @Override
-   public Object getProperty(String name) {
-      ArgsCheck.notEmpty("name", name);
-      return properties.get(name);
-   }
+    /**
+     * {@inheritDoc)
+     *
+     */
+    @Override
+    public Object getProperty(String name) {
+        ArgsCheck.notEmpty("name", name);
+        return properties.get(name);
+    }
 
-   /**
-    * {@inheritDoc)
-    */
-   @Override
-   public void setProperty(String name, Object val) {
-      set(name, val);
-   }
+    /**
+     * {@inheritDoc)
+     */
+    @Override
+    public void setProperty(String name, Object val) {
+        set(name, val);
+    }
 
-   /**
-    * Will check if the given value is null it will remove the last mapping from
-    * the map, otherwise it will put the mapping.
-    * 
-    * @param name
-    * @param val
-    * @return
-    */
-   protected Object set(String name, Object val) {
-      ArgsCheck.notEmpty("name", name);
-      if (val == null) {
-         return properties.remove(name);
-      } else {
-         return properties.put(name, val);
-      }
-   }
+    /**
+     * Will check if the given value is null it will remove the last mapping
+     * from the map, otherwise it will put the mapping.
+     *
+     * @param name
+     * @param val
+     * @return
+     */
+    protected Object set(String name, Object val) {
+        ArgsCheck.notEmpty("name", name);
+        if (val == null) {
+            return properties.remove(name);
+        } else {
+            return properties.put(name, val);
+        }
+    }
 
-   public void merge(ConfigDomain domain) {
-   }
+    public void merge(ConfigDomain domain) {
+    }
 }
